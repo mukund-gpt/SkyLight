262c9a5c2b2a0b7a7f16c73d93a1ffc0c66a2a3a
Prakhar-Garg7 <prakhargarg1407@gmail.com>
2024-04-08 20:26:53 +0530

    user location dependency and permissions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,5 +55,6 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    implementation ("com.google.android.gms:play-services-location:21.2.0")
 
 }
