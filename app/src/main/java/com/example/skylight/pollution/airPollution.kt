a214c45dded729a5304f8b8ab136d7caad3fc294
mukund-gpt <abhishek.jupyter@gamil.com>
2024-04-10 15:40:23 +0530

    pollution api first part done

---
+++ app/src/main/java/com/example/skylight/pollution/airPollution.kt
@@ -0,0 +1,32 @@
+package com.example.skylight.pollution
+
+data class airPollution(
+    val coord: Coord,
+    val list: ArrayList<Items>
+) {
+    data class Coord(
+        val lat: Int,
+        val lon: Int
+    )
+
+    data class Items (
+        val components: Components,
+        val dt: Int,
+        val main: Main
+    ) {
+        data class Components(
+            val co: Double,
+            val nh3: Double,
+            val no: Double,
+            val no2: Double,
+            val o3: Double,
+            val pm10: Double,
+            val pm2_5: Double,
+            val so2: Double
+        )
+
+        data class Main(
+            val aqi: Int
+        )
+    }
+}
